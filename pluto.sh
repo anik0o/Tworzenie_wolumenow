fa58c63fb0509221d7d4eec241bc8747a367e494
anik0o <weronika.mr2@gmail.com>
2021-11-27 23:22:41 +0100

    Update pluto.sh

--- pluto.sh
+++ pluto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Generowanie czasu uruchomienia
-date '+%Y-%m-%d %H:%M:%S' | awk '{ date =$1; time=$2; print "Data i czas: " date " " time}' > /logi/info.log
+date '+%Y-%m-%d %H:%M:%S' | awk '{ date =$1; time=$2; print "Data i czas: " date " " time}' > info.log
 
 #Zapis informacji odnosnie zuzycia pamieci
-cat /sys/fs/cgroup/memory/memory.usage_in_bytes  | awk '{ byte =$1 /1024/1024; print "Pamiec: " byte " MB" }' >> /logi/info.log 
+cat /sys/fs/cgroup/memory/memory.usage_in_bytes  | awk '{ byte =$1 /1024/1024; print "Pamiec: " byte " MB" }' >> info.log 
